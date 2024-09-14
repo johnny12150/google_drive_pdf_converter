b9508d2520eec43e01f4ea59068b08a7d61a0111
wade <johnny12150@gmail.com>
2024-09-14 21:44:01 +0800

    Download a folder from google drive link

---
+++ main.py
@@ -0,0 +1,29 @@
+import os
+import shutil
+from pathlib import Path
+
+import gdown
+
+
+def download_single_file(url, destination):
+    gdown.download(url, destination)
+
+
+def download_a_folder(url, destination):
+    file_list = gdown.download_folder(url)
+
+    # Move download files to given place
+    for file in file_list:
+        if not os.path.isdir(file):
+            shutil.move(file, destination)
+
+    # Delete the source folder
+    for file in file_list:
+        if os.path.exists(Path(file).parent):
+            shutil.rmtree(Path(file).parent)
+            print(f"Remove folder: {str(Path(file).parent)}")
+
+if __name__ == '__main__':
+    # Handle a folder
+    download_a_folder('https://drive.google.com/drive/folders/1iM2Pt85FQeNyXtm1vRGbEhHlRvufbBLe?usp=sharing',
+                         'download')

24d921966d850b337c289b7fe2e5b4cd0d15a709
wade <johnny12150@gmail.com>
2024-09-14 22:14:23 +0800

    Automatically get file name

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 import os
 import shutil
+import requests
 from pathlib import Path
 
+import bs4
 import gdown
 
 
+def get_file_name(url):
+    res = requests.get(url)
+    html = bs4.BeautifulSoup(res.text, features="html.parser")
+    html_title = html.title.get_text()
+    return html_title.split(" - Google 雲端硬碟")[0]
+
+
 def download_single_file(url, destination):
-    gdown.download(url, destination, fuzzy=True)
+    file_name = get_file_name(url)
+
+    gdown.download(url, f'{destination}/{file_name}', fuzzy=True)
 
 
 def download_a_folder(url, destination):
@@ -29,7 +40,7 @@ def download_a_folder(url, destination):
 if __name__ == '__main__':
     # Handle a file link (preview page)
     download_single_file('https://drive.google.com/file/d/13ZBJ6P_UvicQFZmuHQ4CumT_8mwvX9YE/view',
-                         'download/test.pdf')
+                         'download')
 
     # Handle a folder
     download_a_folder('https://drive.google.com/drive/folders/1iM2Pt85FQeNyXtm1vRGbEhHlRvufbBLe?usp=sharing',

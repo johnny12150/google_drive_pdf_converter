581d8a54435a76a69b291728e3fc0d24c8e09e71
wade <johnny12150@gmail.com>
2024-09-15 20:51:45 +0800

    Add 2 more UT

--- tests/test_download.py
+++ tests/test_download.py
@@ -5,13 +5,15 @@ from utils.downloader import get_file_name, download_single_file, download_a_fol
 
 class TestGoogleDriveDownloader(unittest.TestCase):
     def test_get_file_name(self):
-        file_name = get_file_name('https://drive.google.com/file/d/13ZBJ6P_UvicQFZmuHQ4CumT_8mwvX9YE/view')
+        get_file_name('https://drive.google.com/file/d/13ZBJ6P_UvicQFZmuHQ4CumT_8mwvX9YE/view')
 
     def test_download_single_file(self):
-        pass
+        download_single_file('https://drive.google.com/file/d/13ZBJ6P_UvicQFZmuHQ4CumT_8mwvX9YE/view',
+                             '.')
 
     def test_download_a_folder(self):
-        pass
+        download_a_folder('https://drive.google.com/drive/folders/1iM2Pt85FQeNyXtm1vRGbEhHlRvufbBLe?usp=sharing',
+                          '.')
 
 
 if __name__ == '__main__':
